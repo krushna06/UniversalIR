cd50331a4711eb9dd1ead1624d30023c874e840b
krushna06 <krushnavyas06@gmail.com>
2026-02-28 00:40:34 +0530

    fix: adjust IRrecv parameters, and improve learning timeout handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
-#include <Wire.h>;
+#include <Wire.h>
 #include <Preferences.h>
 #include <U8g2lib.h>
 
@@ -10,6 +10,7 @@
 #include <IRrecv.h>
 #include <IRutils.h>
 #include <ir_Toshiba.h>
+#include <IRac.h>
 
 // ============================
 // CONFIGURATION
@@ -22,14 +23,13 @@
 const char* SSID = "";
 const char* PASSWORD = "";
 
-WebServer server(80);
-IRsend irsend(IR_LED_PIN);
-IRrecv irrecv(IR_RECV_PIN, 1024, 50, true);
+IRrecv irrecv(IR_RECV_PIN, 2600, 50, true);
 decode_results results;
-
-Preferences prefs;
-
+IRsend irsend(IR_LED_PIN);
 IRToshibaAC ac(IR_LED_PIN);
+
+WebServer server(80);
+Preferences prefs;
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C oled(U8G2_R0, U8X8_PIN_NONE);
 
@@ -53,7 +53,7 @@ body { font-family:Arial; background:#f0f0f0; text-align:center; }
 .btn-green { background:#5CB85C; }
 .btn-small { width:45%; display:inline-block; }
 .title { font-size:22px; margin-bottom:10px; }
-input { width:80%; padding:10px; margin:10px; font-size:18px; }
+textarea { width:95%; height:100px; }
 </style>
 )";
 
@@ -73,8 +73,9 @@ String pageAC() {
   return css + R"(
   <div class='section'>
      <div class='title'>Toshiba AC</div>
+
      <a class='btn-green btn' href='/ac_on'>POWER ON</a>
-     <a class='btn-red btn' href='/ac_off'>POWER OFF</a>
+     <a class='btn-red btn'   href='/ac_off'>POWER OFF</a>
 
      <a class='btn-small btn' href='/ac_temp_up'>TEMP +</a>
      <a class='btn-small btn' href='/ac_temp_down'>TEMP -</a>
@@ -86,35 +87,6 @@ String pageAC() {
      <a class='btn' style='background:#777' href='/'>Back</a>
   </div>
 )";
-}
-
-void acSend(String msg) {
-  showOLED(msg);
-  Serial.println(ac.toString());
-  ac.send();
-}
-
-struct RGBButton {
-  const char* name;
-  uint32_t code;
-};
-
-RGBButton rgbButtons[] = {
-  {"ON",    0x00F7C03F},
-  {"OFF",   0x00F740BF},
-  {"R",     0x00F720DF},
-  {"G",     0x00F7A05F},
-  {"B",     0x00F7609F},
-  {"WHITE", 0x00F7E01F},
-  {"FLASH", 0x00F7D02F},
-  {"STROBE",0x00F7F00F},
-  {"FADE",  0x00F7C837},
-  {"SMOOTH",0x00F7E817},
-};
-
-void sendRGB(uint32_t code, String msg) {
-  showOLED(msg);
-  irsend.sendNEC(code, 32);
 }
 
 String pageRGB() {
@@ -145,7 +117,7 @@ String pageLearn() {
   return css + R"(
   <div class='section'>
     <div class='title'>Learn IR Button</div>
-    <p>Point your remote at the receiver<br>and press a button now…</p>
+    <p>Point your remote at the receiver<br>and press a button…</p>
     <a class='btn' href='/start_learn'>Start Learning</a>
     <a class='btn' style='background:#777' href='/'>Back</a>
   </div>
@@ -159,16 +131,12 @@ String pageLearned() {
   size_t count = prefs.getUInt("count", 0);
   prefs.end();
 
-  if (count == 0) {
-    html += "<p>No saved buttons</p>";
-  } else {
+  if (count == 0) html += "<p>No saved buttons</p>";
+  else {
     prefs.begin("irlearn", true);
-
     for (int i = 0; i < count; i++) {
       String key = "btn" + String(i);
       String name = prefs.getString((key + "_name").c_str(), "");
-      String code = prefs.getString((key + "_code").c_str(), "");
-
       html += "<p><b>" + name + "</b><br>";
       html += "<a class='btn-small btn' href='/send_learned?id=" + String(i) + "'>Send</a>";
       html += "<a class='btn-small btn-red btn' href='/delete_learned?id=" + String(i) + "'>Delete</a></p>";
@@ -183,21 +151,29 @@ String pageLearned() {
 void startLearning() {
   showOLED("Learning...");
   irrecv.enableIRIn();
+  irrecv.setTolerance(40);
 
   unsigned long start = millis();
-  while (millis() - start < 10000) {
-    if (irrecv.decode(&results)) {
 
-      String rawHex = uint64ToString(results.value, HEX);
+  while (millis() - start < 8000) {
+    if (irrecv.decode(&results)) {
+      Serial.println(resultToHumanReadableBasic(&results));
+
+      String rawData = "";
+      for (uint16_t i = 1; i < results.rawlen; i++) {
+        rawData += String(results.rawbuf[i] * kRawTick);
+        if (i < results.rawlen - 1) rawData += ",";
+      }
 
       irrecv.resume();
+      showOLED("Captured");
 
       String html = css;
       html += "<div class='section'><div class='title'>New Code Captured</div>";
-      html += "<p>Hex: " + rawHex + "</p>";
+      html += "<p>Raw Signal:</p><textarea>" + rawData + "</textarea>";
       html += "<form action='/save_learned'>";
       html += "<input name='name' placeholder='Button Name'><br>";
-      html += "<input type='hidden' name='code' value='" + rawHex + "'>";
+      html += "<input type='hidden' name='code' value='" + rawData + "'>";
       html += "<input type='submit' class='btn' value='Save'>";
       html += "</form>";
       html += "<a class='btn' style='background:#777' href='/'>Cancel</a></div>";
@@ -207,18 +183,17 @@ void startLearning() {
     }
   }
 
-  server.send(200, "text/html", "<h1>Timeout - No IR Detected</h1><a href='/'>Back</a>");
+  server.send(200, "text/html", "<h1>Timeout - No IR detected</h1><a href='/'>Back</a>");
 }
 
 void saveLearned(String name, String code) {
   prefs.begin("irlearn", false);
 
   size_t count = prefs.getUInt("count", 0);
-
   prefs.putString(("btn" + String(count) + "_name").c_str(), name);
   prefs.putString(("btn" + String(count) + "_code").c_str(), code);
-
   prefs.putUInt("count", count + 1);
+
   prefs.end();
 
   showOLED("Saved");
@@ -231,15 +206,24 @@ void sendLearned(int id) {
 
   String key = "btn" + String(id);
   String name = prefs.getString((key + "_name").c_str(), "");
-  String hex = prefs.getString((key + "_code").c_str(), "");
+  String raw = prefs.getString((key + "_code").c_str(), "");
 
   prefs.end();
 
   showOLED(name);
 
-  uint64_t code = strtoull(hex.c_str(), NULL, 16);
-  irsend.sendNEC(code, 32);
-  
+  std::vector<uint16_t> pulses;
+  String temp = "";
+  for (size_t i = 0; i < raw.length(); i++) {
+    if (raw[i] == ',') {
+      pulses.push_back(temp.toInt());
+      temp = "";
+    } else temp += raw[i];
+  }
+  if (temp.length() > 0) pulses.push_back(temp.toInt());
+
+  irsend.sendRaw(pulses.data(), pulses.size(), 38);
+
   server.sendHeader("Location", "/learned");
   server.send(303);
 }
@@ -251,9 +235,9 @@ void deleteLearned(int id) {
 
   for (int i = id; i < count - 1; i++) {
     prefs.putString(("btn" + String(i) + "_name").c_str(),
-                    prefs.getString(("btn" + String(i+1) + "_name").c_str(), ""));
+                    prefs.getString(("btn" + String(i + 1) + "_name").c_str(), ""));
     prefs.putString(("btn" + String(i) + "_code").c_str(),
-                    prefs.getString(("btn" + String(i+1) + "_code").c_str(), ""));
+                    prefs.getString(("btn" + String(i + 1) + "_code").c_str(), ""));
   }
 
   prefs.putUInt("count", count - 1);
@@ -263,33 +247,42 @@ void deleteLearned(int id) {
   server.send(303);
 }
 
+void acSend(String msg) {
+  showOLED(msg);
+  Serial.println(ac.toString());
+  ac.send();
+}
+
 void setupRoutes() {
+
   server.on("/", [](){ server.send(200, "text/html", pageHome()); });
   server.on("/ac", [](){ server.send(200, "text/html", pageAC()); });
 
   server.on("/ac_on", [](){ ac.on(); acSend("AC ON"); server.sendHeader("Location","/ac"); server.send(303); });
   server.on("/ac_off", [](){ ac.off(); acSend("AC OFF"); server.sendHeader("Location","/ac"); server.send(303); });
-  server.on("/ac_temp_up", [](){ ac.setTemp(ac.getTemp()+1); acSend("TEMP UP"); server.sendHeader("Location","/ac"); server.send(303); });
-  server.on("/ac_temp_down", [](){ ac.setTemp(ac.getTemp()-1); acSend("TEMP DOWN"); server.sendHeader("Location","/ac"); server.send(303); });
+
+  server.on("/ac_temp_up", [](){ ac.setTemp(ac.getTemp()+1); acSend("TEMP +"); server.sendHeader("Location","/ac"); server.send(303); });
+  server.on("/ac_temp_down", [](){ ac.setTemp(ac.getTemp()-1); acSend("TEMP -"); server.sendHeader("Location","/ac"); server.send(303); });
+
   server.on("/ac_mode", [](){ ac.setMode(kToshibaAcCool); acSend("MODE COOL"); server.sendHeader("Location","/ac"); server.send(303); });
-  server.on("/ac_fan", [](){ ac.setFan(kToshibaAcFanAuto); acSend("FAN"); server.sendHeader("Location","/ac"); server.send(303); });
+  server.on("/ac_fan", [](){ ac.setFan(kToshibaAcFanAuto); acSend("FAN AUTO"); server.sendHeader("Location","/ac"); server.send(303); });
   server.on("/ac_swing", [](){ ac.setSwing(true); acSend("SWING"); server.sendHeader("Location","/ac"); server.send(303); });
 
   server.on("/rgb", [](){ server.send(200, "text/html", pageRGB()); });
 
-  server.on("/rgb_on", [](){ sendRGB(0x00F7C03F, "RGB ON"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_off", [](){ sendRGB(0x00F740BF, "RGB OFF"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_on", [](){ irsend.sendNEC(0x00F7C03F,32); showOLED("RGB ON"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_off", [](){ irsend.sendNEC(0x00F740BF,32); showOLED("RGB OFF"); server.sendHeader("Location","/rgb"); server.send(303); });
 
-  server.on("/rgb_r", [](){ sendRGB(0x00F720DF, "RED"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_g", [](){ sendRGB(0x00F7A05F, "GREEN"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_b", [](){ sendRGB(0x00F7609F, "BLUE"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_r", [](){ irsend.sendNEC(0x00F720DF,32); showOLED("RED"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_g", [](){ irsend.sendNEC(0x00F7A05F,32); showOLED("GREEN"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_b", [](){ irsend.sendNEC(0x00F7609F,32); showOLED("BLUE"); server.sendHeader("Location","/rgb"); server.send(303); });
 
-  server.on("/rgb_white", [](){ sendRGB(0x00F7E01F, "WHITE"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_white", [](){ irsend.sendNEC(0x00F7E01F,32); showOLED("WHITE"); server.sendHeader("Location","/rgb"); server.send(303); });
 
-  server.on("/rgb_flash", [](){ sendRGB(0x00F7D02F, "FLASH"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_strobe", [](){ sendRGB(0x00F7F00F, "STROBE"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_fade", [](){ sendRGB(0x00F7C837, "FADE"); server.sendHeader("Location","/rgb"); server.send(303); });
-  server.on("/rgb_smooth", [](){ sendRGB(0x00F7E817, "SMOOTH"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_flash", [](){ irsend.sendNEC(0x00F7D02F,32); showOLED("FLASH"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_strobe", [](){ irsend.sendNEC(0x00F7F00F,32); showOLED("STROBE"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_fade", [](){ irsend.sendNEC(0x00F7C837,32); showOLED("FADE"); server.sendHeader("Location","/rgb"); server.send(303); });
+  server.on("/rgb_smooth", [](){ irsend.sendNEC(0x00F7E817,32); showOLED("SMOOTH"); server.sendHeader("Location","/rgb"); server.send(303); });
 
   server.on("/learn", [](){ server.send(200, "text/html", pageLearn()); });
   server.on("/start_learn", [](){ startLearning(); });
@@ -318,13 +311,15 @@ void setup() {
 
   irsend.begin();
   irrecv.enableIRIn();
+  irrecv.setTolerance(40);
 
   ac.begin();
   ac.setMode(kToshibaAcCool);
   ac.setTemp(24);
 
   WiFi.begin(SSID, PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) delay(300);
+  while (WiFi.status() != WL_CONNECTED) delay(200);
+
   showOLED("WiFi Ready");
 
   setupRoutes();
